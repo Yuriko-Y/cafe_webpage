685f89e206162eef3dc64e22192e30a080407e54
Yuriko-Y <59592510+Yuriko-Y@users.noreply.github.com>
2020-05-30 16:12:50 +0900

    メニューのアニメーション

--- js/main.js
+++ js/main.js
@@ -9,9 +9,31 @@ $(function(){
        } else {
             pagetop.fadeOut();
        }
+
     });
     pagetop.click(function () {
        $('body, html').animate({ scrollTop: 0 }, 500);
        return false;
     });
+    // フェードイン
+    function animation(){
+        $('.item').each(function(){
+          //ターゲットの位置を取得
+          var target = $(this).offset().top;
+          //スクロール量を取得
+          var scroll = $(window).scrollTop();
+          //ウィンドウの高さを取得
+          var windowHeight = $(window).height();
+          //ターゲットまでスクロールするとフェードインする
+          if (scroll > target - windowHeight){
+            $(this).css('opacity','1');
+            $(this).css('transform','translateY(0)');
+          }
+        });
+      }
+      animation();
+      $(window).scroll(function (){
+        animation();
+      });
   });
+  
